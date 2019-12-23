73b156f9d1750169c112c8f634f6689cab5e6150
tms_ych <1925118530@qq.com>
2019-12-23 10:32:03 +0800

    登陆界面效果

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -9,7 +9,7 @@
         </div>
       </div>
       <div class="login_content">
-        <form>
+        <form @submit.prevent="login">
           <div :class="{on:loginWay}">
             <section class="login_message">
               <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
@@ -19,7 +19,7 @@
               </button>
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="验证码">
+              <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
             </section>
             <section class="login_hint">
               温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
@@ -29,7 +29,7 @@
           <div :class="{on:!loginWay}">
             <section>
               <section class="login_message">
-                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                <input type="text" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
               </section>
               <section class="login_verification">
 
@@ -41,7 +41,7 @@
                 </div>
               </section>
               <section class="login_message">
-                <input type="text" maxlength="11" placeholder="验证码">
+                <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
                 <img class="get_verification" src="./images/captcha.svg" alt="captcha">
               </section>
             </section>
@@ -54,10 +54,12 @@
         <i class="iconfont icon-jiantou2"></i>
       </a>
     </div>
+    <AlertTip :alert-text="alertText" v-show="alertShow" @closeTip="closeTip"></AlertTip>
   </section>
 </template>
 
 <script>
+  import AlertTip from '../../components/AlertTip/AlertTip.vue'
   export default {
     data(){
       return{
@@ -65,7 +67,12 @@
         computeTime:0,//计时的时间
         showPwd:false,//是否显示密码
         phone:'',//手机号
+        code:'',//短信验证码
+        name:'',//用户名
         pwd:'',//密码
+        captcha:'',//图形验证码
+        alertText: '',//提示文本
+        alertShow:false,//是否显示提示框
       }
     },
     computed:{
@@ -73,26 +80,66 @@
         return /^1\d{10}$/.test(this.phone)
       }
     },
-    methods:{
-      getCode(){
+    methods: {
+      //异步获取短信验证码
+      getCode() {
         //如果当前没有计时
-        if(!this.computeTime){
+        if (!this.computeTime) {
           //启动倒计时
           this.computeTime = 30
-          const intervalId = setInterval(()=>{
+          const intervalId = setInterval(() => {
             this.computeTime--
-            if(this.computeTime<=0){
+            if (this.computeTime <= 0) {
               //停止计时
               clearInterval(intervalId)
             }
-          },1000)
+          }, 1000)
           //发送Ajax请求向指定手机号发送验证码短信
 
         }
 
 
+      },
+      showAlert(alertText){
+        this.alertShow = true
+        this.alertText =alertText
+      },
+      //异步登陆
+      login() {
+        //前台表单验证
+        if (this.loginWay) {//短信登陆
+          const {rightPhone, phone, code} = this
+          if (!this.rightPhone) {
+            //手机号不正确
+            this.showAlert('手机号不正确')
 
+          } else if (!/^\d{6}$/.test(code)) {
+            //验证码必须是6位数字
+            this.showAlert('验证码必须是6位数字')
+
+          }
+        } else {//密码登录
+          const {name, pwd, captcha} = this
+          if (!this.name) {
+            //用户名必须指定
+            this.showAlert('用户名必须指定')
+          } else if (!this.pwd) {
+            //密码必须指定
+            this.showAlert('密码必须指定')
+          } else if (!this.captcha) {
+            //验证码必须指定
+            this.showAlert('验证码必须指定')
+          }
+
+        }
+      },
+      closeTip(){
+        this.alertShow = false
+        this.alertText =''
       }
+    },
+    components:{
+      AlertTip
     }
   }
 </script>
