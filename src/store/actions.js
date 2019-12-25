337fa2e7c3852e6b9023d6f7bde479a0e68db0bb
tms_ych <1925118530@qq.com>
2019-12-25 15:09:15 +0800

    浏览器记住登录

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,7 +8,8 @@ import {
 import {
   reqAddress,
   reqFoodCategorys,
-  reqShops
+  reqShops,
+  reqUserInfo
 } from '../api'
 export default {
   //异步获取地址
@@ -47,5 +48,13 @@ export default {
   //同步记录用户信息
   recordUser ({commit},userInfo){
     commit(RECEIVE_USER_INFO,{userInfo})
+  },
+  //异步获取用户信息
+  async getUserInfo({commit}){
+    const result = await reqUserInfo()
+    if(result.code===0){
+      const userInfo =result.data
+      commit(RECEIVE_USER_INFO,{userInfo})
+    }
   }
 }
