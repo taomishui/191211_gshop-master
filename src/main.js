ab1c7c05a5041223caa8c58da61c3a56f7e24046
tms_ych <1925118530@qq.com>
2020-01-06 20:39:00 +0800

    日期格式化

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import router from './router'
 import store from './store'
 import './mock/mockServer.js'
 import loading from './common/imgs/loading.gif'
+import './filters' //加载过滤器
 //注册全局组件标签
 Vue.component(Button.name,Button)//<mt-button>
 Vue.use(VueLazyload, {//内部自定义一个指令lazy
--- src/pages/Shop/ShopRatings/ShopRatings.vue
+++ src/pages/Shop/ShopRatings/ShopRatings.vue
@@ -63,7 +63,7 @@
                 <span class="item" v-for="(item,index) in rating.recommend" :key="index">{{item}}</span>
 
               </div>
-              <div class="time">{{rating.rateTime}}</div>
+              <div class="time">{{rating.rateTime | date-format}}</div>
             </div>
           </li>
 
