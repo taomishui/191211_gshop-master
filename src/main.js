8bd102b5953fd0839827f6c0c2d3f92cf1cd15c0
tms_ych <1925118530@qq.com>
2020-01-06 20:29:31 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@
 
 import Vue from 'vue'
 import {Button} from 'mint-ui'
+import VueLazyload from 'vue-lazyload'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './mock/mockServer.js'
-
+import loading from './common/imgs/loading.gif'
 //注册全局组件标签
 Vue.component(Button.name,Button)//<mt-button>
+Vue.use(VueLazyload, {//内部自定义一个指令lazy
+
+  loading
+
+})
 
 new Vue({
   el:'#app',
